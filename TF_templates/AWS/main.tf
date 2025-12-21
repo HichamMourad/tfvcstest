9909de10a93189cc54da69e4b82d4a3c4e4efa3a
Hicham Mourad <hicham.mourad@gmail.com>
2025-12-21 00:16:18 -0500

    test without vault

--- TF_templates/AWS/main.tf
+++ TF_templates/AWS/main.tf
@@ -22,15 +22,18 @@ terraform {
 }
 
 # Data source to retrieve AWS credentials from Vault
-data "vault_generic_secret" "aws_creds" {
-  path = "secret/aws_creds"
-}
+#
+#data "vault_generic_secret" "aws_creds" {
+#  path = "secret/data/aws_creds"
+#}
 
 # Provider configuration for AWS (credentials from Vault)
 provider "aws" {
   region     = var.aws_region
-  access_key = data.vault_generic_secret.aws_creds.data.access_key
-  secret_key = data.vault_generic_secret.aws_creds.data.secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+#  access_key = data.vault_generic_secret.aws_creds.data.access_key
+#  secret_key = data.vault_generic_secret.aws_creds.data.secret_key
 }
 
 # Provider configuration for Vault using AppRole authentication
